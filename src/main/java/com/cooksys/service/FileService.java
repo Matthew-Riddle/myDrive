c3b2f98d422a44ed11e099adbb5143d62565e418
Cruz Muniz <kingcruziii@gmail.com>
2018-07-10 11:30:53 -0500

    created function names

---
+++ src/main/java/com/cooksys/service/FileService.java
@@ -0,0 +1,35 @@
+package com.cooksys.service;
+
+import java.util.ArrayList;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class FileService {
+
+	
+	public FileService() {
+		
+	}
+	
+	public FileDto createFile(){
+		
+	}
+	
+	public FileDto getFileById(Long id) {
+		
+	}
+	
+	public ArrayList<FileDto> getFiles() {
+		
+	}
+	
+	public FileDto updateFileById(Long id) {
+		
+	}
+	
+	public FileDto deleteFileById(Long Id) {
+		
+	}
+	
+}
