4d2d238410b388a5b65057ec9b72ba7c9300c17d
Cruz Muniz <kingcruziii@gmail.com>
2018-07-10 10:28:25 -0500

    finished base project

--- src/main/java/com/cooksys/mydrive/MydriveApplication.java
+++ src/main/java/com/cooksys/mydrive/MydriveApplication.java
@@ -3,7 +3,11 @@ package com.cooksys.mydrive;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.annotations.SwaggerDefinition;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication
+@EnableSwagger2
 public class MydriveApplication {
 
 	public static void main(String[] args) {
