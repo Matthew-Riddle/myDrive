7e543654a2bdbb7c29d62222930c13f5ece9b808
Patrick Doane <patrickdoane@gmail.com>
2018-07-10 19:23:41 -0500

    Implement FolderMapper

--- src/main/java/com/cooksys/mapper/FolderMapper.java
+++ src/main/java/com/cooksys/mapper/FolderMapper.java
@@ -1,5 +1,12 @@
 package com.cooksys.mapper;
 
-public class FolderMapper {
+import org.mapstruct.Mapper;
 
+import com.cooksys.dto.FolderDto;
+import com.cooksys.entity.FolderEntity;
+
+@Mapper(componentModel = "spring")
+public interface FolderMapper {
+	FolderDto toDto(FolderEntity entity);
+	FolderEntity toFolder(FolderDto dto);
 }
