2dad65245f3d79c2aac0bdffe2ce1889bd9a0f87
emscook <39387338+emscook@users.noreply.github.com>
2018-07-11 15:25:28 -0500

    Update FileController.java

--- src/main/java/com/cooksys/mydrive/controller/FileController.java
+++ src/main/java/com/cooksys/mydrive/controller/FileController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cooksys.mydrive.dto.FileDto;
@@ -36,7 +35,7 @@ public class FileController {
 	}
 	
 	@PostMapping
-	public FileDto createNewFile(@RequestParam("file") FileDto file) {
+	public FileDto createNewFile(@RequestBody FileDto file) {
 		return fileService.createFile(file);
 	}
 	
