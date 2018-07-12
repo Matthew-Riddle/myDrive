f3d320c80f23db1f847bee4d241b7e13a9a5c944
Patrick Doane <patrickdoane@gmail.com>
2018-07-12 15:37:26 -0500

    Implement CSS Baseline. Remove extraneous styles from App and index. Closes #60

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react'
 import './App.css'
+import { CssBaseline } from '@material-ui/core'
 
 class App extends Component {
   render () {
     return (
-      <div className='App'>
-        <header className='App-header'>
-          <h1>myDrive</h1>
-        </header>
-      </div>
+      <React.Fragment>
+        <CssBaseline />
+        <div className='App'>
+          <header className='App-header'>
+            <h1>myDrive</h1>
+          </header>
+        </div>
+      </React.Fragment>
     )
   }
 }
