cae0b5f8c515b2e52cc3b9ac1a97b869a0e28bbb
Matthew-Riddle <Matthew.B.Riddle@gmail.com>
2018-07-13 14:09:17 -0500

    Added redux store
    Added all parts to the redux store. Untested at this point.

---
+++ ui/src/store/actions/fileActions.js
@@ -0,0 +1,62 @@
+import axios from '../../axios-instance'
+
+export const GET_FILE = 'GET_FILE'
+export const GET_FILES = 'GET_FILES'
+export const UPDATE_FILE = 'UPDATE_FILE'
+export const CREATE_FILE = 'CREATE_FILE'
+export const DELETE_FILE = 'DELETE_FILE'
+
+const getFile = file => ({
+  type: GET_FILE,
+  file
+})
+
+export const getFileAsync = () => dispatch => {
+  axios.get('/index.json').then(response => {
+    dispatch(getFile(response))
+  })
+}
+
+const getFiles = files => ({
+  type: GET_FILES,
+  files
+})
+
+export const getFilesAsync = () => dispatch => {
+  axios.get('/index.json').then(response => {
+    dispatch(getFiles(response))
+  })
+}
+
+const updateFile = file => ({
+  type: UPDATE_FILE,
+  file
+})
+
+export const updateFileAsync = () => dispatch => {
+  axios.get('/index.json').then(response => {
+    dispatch(updateFile(response))
+  })
+}
+
+const createFile = file => ({
+  type: CREATE_FILE,
+  file
+})
+
+export const createFileAsync = () => dispatch => {
+  axios.get('/index.json').then(response => {
+    dispatch(createFile(response))
+  })
+}
+
+const deleteFile = file => ({
+  type: DELETE_FILE,
+  file
+})
+
+export const deleteFileAsync = () => dispatch => {
+  axios.get('/index.json').then(response => {
+    dispatch(deleteFile(response))
+  })
+}
---
+++ ui/src/store/actions/index.js
@@ -0,0 +1,18 @@
+export {
+  GET_FILE,
+  UPDATE_FILE,
+  CREATE_FILE,
+  DELETE_FILE,
+  getFileAsync,
+  updateFileAsync,
+  createFileAsync,
+  deleteFileAsync,
+  GET_FOLDER,
+  UPDATE_FOLDER,
+  CREATE_FOLDER,
+  DELETE_FOLDER,
+  getFolderAsync,
+  updateFolderAsync,
+  createFolderAsync,
+  deleteFolderAsync
+} from './actions'
