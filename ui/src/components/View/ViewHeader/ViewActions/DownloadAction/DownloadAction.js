bb70df5157474cb3302b7211fe247844e5b3b4b6
Matthew-Riddle <Matthew.B.Riddle@gmail.com>
2018-07-13 10:51:52 -0500

    ViewActionsComponent Closes #50 Fixes #52
    Implemented view actions component. Had to change #52 trash action from a div container to a button in order to match the download icon.

--- ui/src/components/View/ViewHeader/ViewActions/DownloadAction/DownloadAction.js
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-import './DownloadAction.css'
-
-const DownloadAction = () => <div className='DownloadAction' />
-
-export default DownloadAction
---
+++ ui/src/components/View/ViewHeader/ViewActions/ViewActions.stories.js
@@ -0,0 +1,5 @@
+import React from 'react'
+import ViewActions from './ViewActions'
+import { storiesOf } from '@storybook/react'
+
+storiesOf('ViewActions', module).add('with children', () => <ViewActions />)
