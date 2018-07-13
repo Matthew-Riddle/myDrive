4811999eee22cec19b96a5462d1a65d575e98227
Unknown <emseduau@gmail.com>
2018-07-13 11:26:06 -0500

    /Folder-Hotfix-Base: Auto stash before merge of "hotfix/Folder-Hotfix-Base" and "origin/develop"

--- ui/src/components/View/ViewFiles/ViewFiles.js
+++ ui/src/components/View/ViewFiles/ViewFiles.js
@@ -1,6 +1,17 @@
 import React from 'react'
 import './ViewFiles.css'
-
-const ViewFiles = () => <div className='ViewFiles' />
-
+import File from './File/File'
+import { GridList, GridListTile } from '@material-ui/core'
+const ViewFiles = props => {
+  const fileWidth = 55
+  return (
+    <GridList cols='auto' cellHeight='auto' spacing={10}>
+      {props.Files.map(slime => (
+        <GridListTile cols={1} style={{ width: `${fileWidth}px` }}>
+          <File name={slime} />
+        </GridListTile>
+      ))}
+    </GridList>
+  )
+}
 export default ViewFiles
