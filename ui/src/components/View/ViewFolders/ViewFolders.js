bae06e8709b5c83a720b0f6590ed0faf2c595554
Patrick Doane <patrickdoane@gmail.com>
2018-07-14 14:19:02 -0500

    Hook ViewFolders to redux store. Closes #71

--- ui/src/components/View/ViewFolders/ViewFolders.js
+++ ui/src/components/View/ViewFolders/ViewFolders.js
@@ -1,30 +1,46 @@
 import React, { Component } from 'react'
-import './ViewFolders.css'
+import * as actionCreators from '../../../store/actions/folderActions'
+import { connect } from 'react-redux'
 import { GridList, Typography, GridListTile } from '@material-ui/core'
+import './ViewFolders.css'
 
 import Folder from './Folder/Folder'
 
 class ViewFolders extends Component {
+  componentDidMount () {
+    this.props.getFolders()
+  }
+
   render () {
     return (
       <Typography>
         <GridList cols='auto' cellHeight='auto' spacing={0}>
-          {this.props.folders.map(folder => (
-            <GridListTile
-              cols={1}
-              style={{
-                width: '110px',
-                textOverflow: 'ellipsis',
-                padding: '0px'
-              }}
-            >
-              <Folder name={folder} />
-            </GridListTile>
-          ))}
+          {this.props.folders
+            ? this.props.folders.map(folder => (
+              <GridListTile
+                cols={1}
+                style={{
+                  width: '110px',
+                  textOverflow: 'ellipsis',
+                  padding: '0px'
+                }}
+                >
+                <Folder name={folder.name} />
+              </GridListTile>
+              ))
+            : ''}
         </GridList>
       </Typography>
     )
   }
 }
 
-export default ViewFolders
+const mapStateToProps = state => ({
+  folders: state.folders.folders
+})
+
+const mapDispatchToProps = dispatch => ({
+  getFolders: () => dispatch(actionCreators.getFoldersAsync())
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(ViewFolders)
