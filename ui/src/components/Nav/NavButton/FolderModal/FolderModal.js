734c8438f326295c565255bfe10798bf08071f67
Patrick Doane <patrickdoane@gmail.com>
2018-07-15 07:56:09 -0500

    Add some style to FolderModal

--- ui/src/components/Nav/NavButton/FolderModal/FolderModal.js
+++ ui/src/components/Nav/NavButton/FolderModal/FolderModal.js
@@ -1,34 +1,58 @@
 import React from 'react'
 import { withStyles } from '@material-ui/core/styles'
-import Modal from '@material-ui/core/Modal'
+import Button from '@material-ui/core/Button'
 import Input from '@material-ui/core/Input'
+import Modal from '@material-ui/core/Modal'
 
 const styles = {
   folderModal: {
     position: 'fixed',
     zIndex: 1,
-    left: '40%',
-    top: '40%',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
     overflow: 'auto',
-    backgroundColor: 'white'
+    backgroundColor: 'white',
+    width: '600px',
+    maxWidth: '100%',
+    height: '400px',
+    maxHeight: '100%'
+  },
+  modalContent: {
+    alignItems: 'center',
+    display: 'flex',
+    height: '100%',
+    justifyContent: 'center',
+    width: '100%'
+  },
+  button: {
+    marginLeft: '8px'
   }
 }
 
 const FolderModal = props => (
   <Modal
+    className={props.classes.folderModal}
+    disableAutoFocus
+    hideBackdrop
     open={props.folderModalOpen}
     onClose={props.handleFolderModalClose}
-    className={props.classes.folderModal}
-    hideBackdrop
   >
-    <div className='modalContent'>
+    <div style={styles.modalContent}>
       <Input
         type='text'
-        className='inputModal'
+        placeholder='Folder name'
         title='Input folder name.'
-        onInput={props.handleFolderChange}
+        onChange={props.handleFolderChange}
       />
-      <Input type='submit' onClick={props.addFolder} />
+      <Button
+        variant='contained'
+        color='grey'
+        style={styles.button}
+        onClick={props.addFolder}
+      >
+        Create
+      </Button>
     </div>
   </Modal>
 )
