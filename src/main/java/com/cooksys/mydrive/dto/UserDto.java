0f52d59a7f77e2fe31579bb90fb0b8a3da22aacd
Cruz Muniz <kingcruziii@live.com>
2018-07-17 10:42:51 -0500

    implemented #113#114
    Added Dtos

---
+++ src/main/java/com/cooksys/mydrive/dto/UserDto.java
@@ -0,0 +1,53 @@
+package com.cooksys.mydrive.dto;
+
+public class UserDto {
+	
+	private Long id;
+	private String UserId;
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((UserId == null) ? 0 : UserId.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		UserDto other = (UserDto) obj;
+		if (UserId == null) {
+			if (other.UserId != null)
+				return false;
+		} else if (!UserId.equals(other.UserId))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getUserId() {
+		return UserId;
+	}
+	public void setUserId(String userId) {
+		UserId = userId;
+	}
+	
+	
+
+}
