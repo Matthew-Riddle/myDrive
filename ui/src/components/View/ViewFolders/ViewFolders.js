7e58b5e7077de049b701cfe5d1ac2cb52f4ebb56
Patrick Doane <patrickdoane@gmail.com>
2018-07-17 16:28:43 -0500

    Only show folders from root directory

--- ui/src/components/View/ViewFolders/ViewFolders.js
+++ ui/src/components/View/ViewFolders/ViewFolders.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import * as actionCreators from '../../../store/actions/folderActions'
 import { connect } from 'react-redux'
-import { GridList, GridListTile } from '@material-ui/core'
+import { GridList, GridListTile, Typography } from '@material-ui/core'
 import './ViewFolders.css'
 
 import Folder from './Folder/Folder'
@@ -15,48 +15,54 @@ class ViewFolders extends Component {
     return (
       <GridList cellHeight='auto' spacing={0}>
         {this.props.folders
-          ? this.props.folders.map(
-              folder =>
-                (this.props.deleted
-                  ? folder.deleted
-                      ? <GridListTile
-                        key={folder.id}
-                        cols={1}
-                        style={{
-                          width: '210px',
-                          textOverflow: 'ellipsis',
-                          padding: '0px'
-                        }}
-                        >
-                        <Folder
-                          name={folder.name}
-                          id={folder.id}
-                          key={folder.id}
-                          deleted={folder.deleted}
-                          folderHandler={this.props.folderHandler}
-                          />
-                      </GridListTile>
-                      : ''
-                  : folder.deleted
-                      ? ''
-                      : <GridListTile
-                        key={folder.id}
-                        cols={1}
-                        style={{
-                          width: '160px',
-                          textOverflow: 'ellipsis',
-                          padding: '0px'
-                        }}
-                        >
-                        <Folder
-                          name={folder.name}
-                          id={folder.id}
-                          key={folder.id}
-                          deleted={folder.deleted}
-                          folderHandler={this.props.folderHandler}
-                          />
-                      </GridListTile>)
-            )
+          ? this.props.folders.filter(
+              folder => (this.props.currentFolder !== null ? '' : folder)
+            ).length > 0
+              ? this.props.folders.map(
+                  folder =>
+                    (this.props.deleted
+                      ? folder.deleted
+                          ? <GridListTile
+                            key={folder.id}
+                            cols={1}
+                            style={{
+                              width: '210px',
+                              textOverflow: 'ellipsis',
+                              padding: '0px'
+                            }}
+                            >
+                            <Folder
+                              name={folder.name}
+                              id={folder.id}
+                              key={folder.id}
+                              deleted={folder.deleted}
+                              folderHandler={this.props.folderHandler}
+                              />
+                          </GridListTile>
+                          : ''
+                      : folder.deleted
+                          ? ''
+                          : <GridListTile
+                            key={folder.id}
+                            cols={1}
+                            style={{
+                              width: '160px',
+                              textOverflow: 'ellipsis',
+                              padding: '0px'
+                            }}
+                            >
+                            <Folder
+                              name={folder.name}
+                              id={folder.id}
+                              key={folder.id}
+                              deleted={folder.deleted}
+                              folderHandler={this.props.folderHandler}
+                              />
+                          </GridListTile>)
+                )
+              : <Typography style={{ paddingLeft: '5px', textAlign: 'left' }}>
+                  No subfolders exist!
+                </Typography>
           : ''}
       </GridList>
     )
